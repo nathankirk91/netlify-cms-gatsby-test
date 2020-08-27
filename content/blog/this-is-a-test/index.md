8f9ca77e6502258c036b3f69deb452a3908ae63c
nathankirk91 <64616413+nathankirk91@users.noreply.github.com>
2020-08-27 20:53:22 +1000

    fixed remark inline images with gatsby-image

--- content/blog/this-is-a-test/index.md
+++
@@ -1,7 +0,0 @@
----
-title: This is a test
-date: 2020-08-22T23:57:55.247Z
-movieTitle: Lord of the Rings
-description: This a test for Lord of the Rings
----
-This a test for Lord of the Rings. More information can go here.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  let featuredImgFluid =
+    data.markdownRemark.frontmatter.featuredImage.childImageSharp.fluid
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <Img fluid={featuredImgFluid} />
+      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    markdownRemark(frontmatter: { title: { eq: "My Favorite Doggos" } }) {
+      html
+      frontmatter {
+        title
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
